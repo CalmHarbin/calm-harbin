36a577b7c118340f53ff64b64f7d7e40e7d5deab
zhanghuan <363639121@qq.com>
2021-08-14 09:36:52 +0800

    upd: 增加build打包兼容

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,18 @@
 const path = require('path')
 // const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
+const CDN_CSS = [
+    // 'https://cdn.jsdelivr.net/npm/calm-harbin@0.2.3-pre/lib/calmHarbin.css'
+]
+const CDN_JS = [
+    'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js',
+    'https://cdn.jsdelivr.net/npm/xlsx@0.17.0/dist/xlsx.min.js',
+    'https://lib.baomitu.com/element-ui/2.13.2/index.js',
+    'https://cdn.jsdelivr.net/npm/umy-ui@1.1.6/lib/index.js',
+    'https://cdn.jsdelivr.net/npm/decimal.js@10.2.0/decimal.min.js',
+    'https://cdn.jsdelivr.net/npm/dayjs@1.10.4/dayjs.min.js'
+]
+
 module.exports = {
     publicPath: './',
     pages: {
@@ -22,34 +34,44 @@ module.exports = {
         // 外部扩展不打包
         externals:
             process.env.NODE_ENV === 'production'
-                ? {
-                      xlsx: {
-                          commonjs: 'xlsx',
-                          amd: 'xlsx',
-                          commonjs2: 'xlsx',
-                          // 这里当使用script模式引入是，该模块指向的全局变量
-                          root: 'XLSX'
-                      },
-                      'element-ui': {
-                          commonjs: 'element-ui',
-                          amd: 'element-ui',
-                          commonjs2: 'element-ui',
-                          root: 'ELEMENT'
-                      },
-                      'umy-ui': {
-                          commonjs: 'umy-ui',
-                          amd: 'umy-ui',
-                          commonjs2: 'umy-ui',
-                          root: 'UMYUI'
-                      },
-                      'decimal.js': {
-                          commonjs: 'decimal.js',
-                          amd: 'decimal.js',
-                          commonjs2: 'decimal.js',
-                          root: 'Decimal'
-                      },
-                      dayjs: 'dayjs'
-                  }
+                ? process.argv.includes('lib')
+                    ? {
+                          xlsx: {
+                              commonjs: 'xlsx',
+                              amd: 'xlsx',
+                              commonjs2: 'xlsx',
+                              // 这里当使用script模式引入时，该模块指向的全局变量
+                              root: 'XLSX'
+                          },
+                          'element-ui': {
+                              commonjs: 'element-ui',
+                              amd: 'element-ui',
+                              commonjs2: 'element-ui',
+                              root: 'ELEMENT'
+                          },
+                          'umy-ui': {
+                              commonjs: 'umy-ui',
+                              amd: 'umy-ui',
+                              commonjs2: 'umy-ui',
+                              root: 'UMYUI'
+                          },
+                          'decimal.js': {
+                              commonjs: 'decimal.js',
+                              amd: 'decimal.js',
+                              commonjs2: 'decimal.js',
+                              root: 'Decimal'
+                          },
+                          dayjs: 'dayjs',
+                          vue: 'Vue'
+                      }
+                    : {
+                          vue: 'Vue',
+                          xlsx: 'XLSX',
+                          'element-ui': 'ELEMENT',
+                          'umy-ui': 'UMYUI',
+                          'decimal.js': 'Decimal',
+                          dayjs: 'dayjs'
+                      }
                 : undefined
     },
     chainWebpack: (config) => {
@@ -60,6 +82,21 @@ module.exports = {
         config.resolve.alias.set('@test', path.resolve(__dirname, './test'))
         config.resolve.alias.set('@types', path.resolve(__dirname, './types'))
         config.resolve.alias.set('@src', path.resolve(__dirname, './src'))
+
+        // app模式使用cdn
+        if (
+            process.env.NODE_ENV === 'production' &&
+            !process.argv.includes('lib')
+        ) {
+            config
+                .plugin('html-index') // 上面pages配置了index，所以是html-index
+                .tap((args) => {
+                    args[0].CDN_CSS = CDN_CSS
+                    args[0].CDN_JS = CDN_JS
+                    return args
+                })
+                .end()
+        }
 
         /** lodash按需加载, 有问题,不用了 */
         // 依赖包 lodash-webpack-plugin babel-plugin-lodash
