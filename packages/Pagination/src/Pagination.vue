d2f53c8736c4c630f74155e1ee14eb132b017a49
zhanghuan <363639121@qq.com>
2021-09-01 09:43:02 +0800

    upd: 去掉scrollTo方法

--- packages/Pagination/src/Pagination.vue
+++ packages/Pagination/src/Pagination.vue
@@ -21,7 +21,6 @@
 </template>
 
 <script>
-import { scrollTo } from './scrollTo'
 import { Pagination } from 'element-ui'
 import { isEqualWith } from '@src/utils/util'
 
@@ -53,10 +52,6 @@ export default {
             default: 'total, sizes, prev, pager, next, jumper, slot'
         },
         background: {
-            type: Boolean,
-            default: true
-        },
-        autoScroll: {
             type: Boolean,
             default: true
         },
@@ -97,15 +92,9 @@ export default {
     methods: {
         handleSizeChange(val) {
             this.$emit('pagination', { page: this.currentPage, limit: val })
-            if (this.autoScroll) {
-                scrollTo(0, 800)
-            }
         },
         handleCurrentChange(val) {
             this.$emit('pagination', { page: val, limit: this.pageSize })
-            if (this.autoScroll) {
-                scrollTo(0, 800)
-            }
         }
     },
     watch: {
