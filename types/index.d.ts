f6496a01334dac06fd526aa23691d6cc47ef5f1d
zhanghuan <363639121@qq.com>
2021-09-13 17:50:45 +0800

    upd: 调整类型声明

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,10 +2,19 @@ export { BiuCard } from './biu-card'
 
 import { PluginFunction } from 'vue'
 
-type calmHarbin<T> = {
-    install: PluginFunction<T>
-}
+// type calmHarbin<T> = {
+//     install: PluginFunction<T>
+// }
 
-export const calmHarbin: calmHarbin<any>
+// export const calmHarbin: calmHarbin<any>
+
+import { debounce, exportExcel, summary } from '../src/utils/index'
+
+declare namespace calmHarbin {
+    export function install<T>(): PluginFunction<T>
+
+    // export const aa: (ops: number) => string
+    export { debounce, exportExcel, summary }
+}
 
 export default calmHarbin
