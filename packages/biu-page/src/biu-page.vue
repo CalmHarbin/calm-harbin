d34fde056b06b3241926ad88f4f2e7625e0dd5fe
zhanghuan <363639121@qq.com>
2021-12-20 23:13:12 +0800

    upd: 增加自定义插槽

--- packages/biu-page/src/biu-page.vue
+++ packages/biu-page/src/biu-page.vue
@@ -11,11 +11,13 @@
             showBtn
         />
         <div style="flex: 1; padding: 0 10px" ref="container">
-            <Operation
-                v-if="customOperationOptions"
-                :operationOptions="customOperationOptions"
-                ref="Operation"
-            />
+            <slot name="operation">
+                <Operation
+                    v-if="customOperationOptions"
+                    :operationOptions="customOperationOptions"
+                    ref="Operation"
+                />
+            </slot>
             <BiuTable
                 ref="BiuTable"
                 v-model="customValue"
@@ -24,30 +26,19 @@
                 v-bind="attrs"
                 v-on="listeners"
             >
-                <!-- 这里会导致BiuTable一直重新渲染, 将slot功能改到render中去 -->
-                <!-- <template
-                    :slot="pagecol.id"
-                    v-for="pagecol in tableColumns"
-                    slot-scope="{ col, row, $index }"
-                >
-                    <slot
-                        :name="'table-' + pagecol.id"
-                        :col="col"
-                        :row="row"
-                        :$index="$index"
-                    ></slot>
-                </template> -->
             </BiuTable>
         </div>
         <!--分页数据-->
-        <Pagination
-            v-if="paginationSync"
-            ref="Pagination"
-            :total="paginationSync.total"
-            :page.sync="page"
-            :limit.sync="size"
-            @pagination="(data) => $emit('pagination', data)"
-        />
+        <slot name="pagination">
+            <Pagination
+                v-if="paginationSync"
+                ref="Pagination"
+                :total="paginationSync.total"
+                :page.sync="page"
+                :limit.sync="size"
+                @pagination="(data) => $emit('pagination', data)"
+            />
+        </slot>
     </div>
 </template>
 
