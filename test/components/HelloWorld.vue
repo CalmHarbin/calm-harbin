135c27c14f5093333126124e3a7bc57500baf8d9
zhanghuan <363639121@qq.com>
2022-03-20 13:29:41 +0800

    :adhesive_bandage:

--- test/components/HelloWorld.vue
+++ test/components/HelloWorld.vue
@@ -124,14 +124,17 @@ export default class HelloWorld extends Vue {
 h3 {
     margin: 40px 0 0;
 }
+
 ul {
     padding: 0;
     list-style-type: none;
 }
+
 li {
     display: inline-block;
     margin: 0 10px;
 }
+
 a {
     color: #42b983;
 }
